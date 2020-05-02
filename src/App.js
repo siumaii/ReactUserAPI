a553a5c9205714fbd19d3df3ff452c1dfd3458a5
siumaii <andysiutang@gmail.com>
2020-05-03 01:12:22 +0200

    Loading message

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import Profile from "./Profile";
-import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
-import About from "./About";
+import Profile from "./Components/Profile";
+import Menu from "./Components/Menu";
 
 function App() {
   const [item, setItem] = useState("");
   const [picture, setPicture] = useState("");
+  const [loading, setLoading] = useState(true);
   async function fetchData() {
     const response = await fetch("https://api.randomuser.me/");
     const data = await response.json();
@@ -14,6 +14,7 @@ function App() {
     const [picture] = data.results;
     setItem(items.name);
     setPicture(picture.picture);
+    setLoading(false);
   }
   useEffect(() => {
     fetchData();
@@ -26,16 +27,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <BrowserRouter>
-      <Link to="/about">About</Link>
-        <Switch>
-          <Route component={About} path="/about" />
-        </Switch>
-        </BrowserRouter>
+       <Menu/>
         <Profile
           title={item.title}
           name={item.first}
           last={item.last}
+          loading={loading}
           picture={picture.large}
         />
         <button onClick={refresh}>Refresh Profile</button>
--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -3,7 +3,7 @@ import React from "react";
 function Profile(props) {
   return (
     <div>
-      <img src={props.picture} alt="logo" />
+     {props.loading ? <div>Loading picture..</div> : <img src={props.picture} alt="logo" />} 
       <p>
         {props.title} {props.name} {props.last}
       </p>
